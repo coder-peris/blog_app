6fe0c2d423b59198e7527c66593f4da4e7b9d7f0
PERIS <periskc@gmail.com>
2025-02-23 15:23:54 +0545

    url_fix

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StoreProvider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/blog_app">
         <Routes>
           <Route path="*" element={<App />} />
         </Routes>
